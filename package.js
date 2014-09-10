52ae9e3a75cc9a08b18926d204a4a2d81a35e088
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-09-10 10:12:26 +0200

    Initial commit

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+	summary: "Connect other oauth accounts with an existing one",
+	version: "0.1.0",
+	git: ""
+});
+
+Package.onUse(function (api) {
+	api.versionsFrom("METEOR@0.9.1.1");
+	api.addFiles("");
+});
